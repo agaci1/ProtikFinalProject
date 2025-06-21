2657eda0fe29d875fe1b6b9ffe15ba51f72faccd
alkeogaci <keogaci@gmail.com>
2025-06-21 12:59:56 +0200

    Initial commit: LuxuryRide project structure

---
+++ LuxuryRideBackend/src/main/java/com/agency/backend/luxuryride/LuxuryRideApplication.java
@@ -0,0 +1,13 @@
+package com.agency.backend.luxuryride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxuryRideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuxuryRideApplication.class, args);
+    }
+
+}
---
+++ LuxuryRideBackend/src/test/java/com/agency/backend/luxuryride/LuxuryRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agency.backend.luxuryride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxuryRideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
